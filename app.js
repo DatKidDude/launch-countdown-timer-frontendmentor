fbfedfa1389e000fa7c4282ee49ffa606624b14f
Craig Giddings <randomhero_345@hotmail.com>
2023-06-03 20:36:30 -0400

    Finished HTML and CSS structuring for flip animation
    Added *.txt to gitignore

---
+++ app.js
@@ -0,0 +1,14 @@
+// let current = 20;
+// let card = document.querySelector(".card");
+
+// setInterval(() => {
+//   let down = --current;
+//   let up = down++;
+//   let data = `
+//     <div class="card-face top flip-top">${down}</div>
+//     <div class="card-face top">${up}</div>
+//     <div class="card-face bottom flip-bottom">${up}</div>
+//     <div class="card-face bottom">${down}</div>
+//   `;
+//   card.innerHTML = data;
+// }, 1000);
